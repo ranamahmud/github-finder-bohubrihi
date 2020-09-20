3a92b1b1ffc03daddae625ba1dd11ed18bb80940
Md Rana Mahmud <ranamd0@gmail.com>
2020-09-20 12:15:34 +0600

    github finder done

--- js/script.js
+++ js/script.js
@@ -1,22 +1,26 @@
 let searchBtn = document.querySelector('#searchBtn');
 let searchUser = document.querySelector('#searchUser');
-let ui = New UI();
+let ui = new UI();
 searchBtn.addEventListener('click', function (e) {
+    console.log("button cliced");
     let userText = searchUser.value;
     if (userText !== '') {
-        // Fetch API 
-        fetch(`http://api.github.com/users/${userText}`)
+        // Fetch API
+        const url = `http://api.github.com/users/${userText}`;
+        fetch(url)
             .then(result => result.json())
             .then(data => {
                 if (data.message == 'Not Found') {
                     // Show Alert
+                    ui.showAlert("User not Found!", "alert alert-danger");
                 } else {
                     // Show Profile
                     ui.showProfile(data);
                 }
             })
-           
+
     } else {
         // Clear Profile
+        ui.clearProfile();
     }
 })
